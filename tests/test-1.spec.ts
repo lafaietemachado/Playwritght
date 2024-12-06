465dc130dc2d92a43c3da3d8ce9a674cbdd58352
Lafaiete Machado <lafaiete.machado@gmail.com>
2024-12-06 10:45:46 -0300

    Atualizando os testes

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test recording', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByLabel('Pesquisar', { exact: true }).click();
-  await page.getByLabel('Pesquisar', { exact: true }).fill('teste com playw');
-  await page.getByText('playwright', { exact: true }).click();
-});
